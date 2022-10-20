c60ff3e722e25e706489d4ca6fc2f741907c51dd
georgeHatem <georgeHatem0@gmail.com>
2022-10-20 16:35:23 -0400

    Implemented tests/fixed cvparam of forecast method

--- forecasthelper.py
+++ forecasthelper.py
@@ -2,7 +2,7 @@
 # Set of helper functions for producing forecasts
 #
 #
-# Author: George Hatem      Last Updated: 2022-10-13
+# Author: George Hatem      Last Updated: 2022-10-19
 #
 
 import re
@@ -16,7 +16,8 @@ from sklearn.multioutput import RegressorChain
 from statsmodels.tsa.deterministic import CalendarFourier, DeterministicProcess
 
 def _fixDate(din: pd.DataFrame) -> pd.DataFrame:
-    """Private method used to check the input for a datetime feature in index and add it if possible."""
+    """Private method used to check the input for a datetime feature in index and add it if possible.
+        This assumes nanoseconds for integer time"""
     if din.empty:
         raise ValueError("Input pandas.DataFrame is empty.")
 
@@ -25,15 +26,15 @@ def _fixDate(din: pd.DataFrame) -> pd.DataFrame:
     if d.index.inferred_type != "datetime64":
         dateLabel = list(filter(re.compile(".*date.*|.*time.*", re.IGNORECASE).match, d.columns.tolist()))
         if len(dateLabel) > 0:
-            d.set_index(dateLabel[0], inplace=True)
+            d = d.set_index(dateLabel[0])
 
     if d.index.inferred_type in ["integer", "string"]:
         try:
-            d.set_index(d.index.to_series().apply(lambda x: pd.to_datetime(x)), inplace=True)
+            d = d.set_index(d.index.to_series().apply(lambda x: pd.to_datetime(x)))
         except:
-            raise TypeError("Index is not DatetimeIndex and could not be parsed into DatetimeIndex.")
+            raise TypeError("Index is not DatetimeIndex and neither it nor a date/time column could be parsed into DatetimeIndex.")
 
-    return d
+    return d.sort_index()
     
 
 
@@ -47,7 +48,7 @@ def detrend(din: pd.DataFrame, target: str = None) -> [pd.DataFrame, LinearRegre
             A single feature should be present unless the target is given.
         - target: (Optional) Name of the feature to detrend
     Output:
-        - (pd.DataFrame, LinearRegression): Single Feature pandas.DataFrame of detrended input, 
+        - [pd.DataFrame, LinearRegression, DeterministicProcess]: Single Feature pandas.DataFrame of detrended input, 
             model describing trend without intercept, statsmodels DeterministicProcess of target feature
     """
     d = _fixDate(din)
@@ -63,8 +64,8 @@ def detrend(din: pd.DataFrame, target: str = None) -> [pd.DataFrame, LinearRegre
             )
 
     if target == None:
-        trendReg = LinearRegression(fit_intercept=False).fit(dp.in_sample(), d[d.columns[0]])
-        dret = d[d.columns[0]] - trendReg.predict(dp.in_sample())
+        trendReg = LinearRegression(fit_intercept=False).fit(dp.in_sample(), d.iloc[:, 0])
+        dret = d.iloc[:, 0] - trendReg.predict(dp.in_sample())
     else:
         trendReg = LinearRegression(fit_intercept=False).fit(dp.in_sample(), d[target])
         dret = d[target] - trendReg.predict(dp.in_sample())
@@ -103,10 +104,10 @@ def deseason(din: pd.DataFrame, season: (str, int), target: str = None) -> [pd.D
             )
 
     if target == None:
-        seasonReg = LinearRegression().fit(dp.in_sample(), d[d.columns[0]])
-        dret = d[0] - seasonReg.predict(dp.in_sample())
+        seasonReg = LinearRegression(fit_intercept=False).fit(dp.in_sample(), d.iloc[:, 0])
+        dret = d.iloc[:, 0] - seasonReg.predict(dp.in_sample())
     else:
-        seasonReg = LinearRegression().fit(dp.in_sample(), d[target])
+        seasonReg = LinearRegression(fit_intercept=False).fit(dp.in_sample(), d[target])
         dret = d[target] - seasonReg.predict(dp.in_sample())
 
     return [pd.DataFrame(dret), seasonReg, dp]
@@ -119,7 +120,8 @@ def multi_shift(din: pd.DataFrame, steps: int, lag_steps: list = [], sign: bool 
         - din: Pandas.DataFrame with feature to lag. 
             Must have a single feature unless the target is given.
         - steps: Number of lags to produce
-        - sign: determines whether to output is a lag or a leads. True shifts forward, False shifts back.
+        - lag_steps: (Optional) List of lag shifts to use. Will use all of range(1, steps + 1) if empty.
+        - sign: (Optional) Determines whether to output is a lag or a leads. True shifts forward, False shifts back.
         - target: (Optional) Name of feature to lag or lead
     """
     d = _fixDate(din)
@@ -142,7 +144,7 @@ def multi_shift(din: pd.DataFrame, steps: int, lag_steps: list = [], sign: bool 
 
     return dret
 
-def forecast(din: pd.DataFrame, length: int, model: object, cvparam: list = None, target: str = None, lags: pd.DataFrame = pd.DataFrame(), leads: pd.DataFrame = pd.DataFrame()) -> [pd.DataFrame, object]:
+def forecast(din: pd.DataFrame, length: int, model: object, cvparam: dict = None, target: str = None, lags: pd.DataFrame = pd.DataFrame(), leads: pd.DataFrame = pd.DataFrame()) -> [pd.DataFrame, RegressorChain]:
     """
     forcast: Returns a pandas.DataFrame of the forecast over an input time series for the given target feature.
         Optionally: input lags and leads pandas.DataFrame
@@ -152,8 +154,8 @@ def forecast(din: pd.DataFrame, length: int, model: object, cvparam: list = None
             Must have a single feature unless the target is given.
         - length: number of time periods to forecast forward
         - model: Instance of modeling algorithm must be base model will be fed to sklearn.multioutput.RegressorChain()
-        - cvparam: (Optional) cross-validation prameters besides the model for sklearn.model_selection.GridSearchCV.
-            eg: "GridSearchCV(model, *cvparam)" where cvparam = [param_grid, cv, scoring].
+        - cvparam: (Optional) cross-validation prameters othre than the model for sklearn.model_selection.GridSearchCV.
+            eg: "GridSearchCV(model, **cvparam)" where cvparam = {param_grid, cv, scoring} as a dictionary.
         - target: (Optional) Name of feature to forcast on
         - lags: (Optional) Minimum 1 lag will be used. This method imputes mean value in NA
         - lag_steps: (Optional) list of lag shifts provided eg: lag_1, lag_2, lag_5 corresponds to [1, 2, 5]
@@ -173,13 +175,13 @@ def forecast(din: pd.DataFrame, length: int, model: object, cvparam: list = None
         feature_name = target
 
     if lags.empty:
-        lags = pd.DataFrame(index=d.index)
+        lags.index = d.index
         lags[feature_name + "_lag_1"] = d[feature_name].shift(1)
 
     lag_length = lags.shape[1]
 
     if leads.empty:
-        leads = pd.DataFrame(index=d.index)
+        leads.index = d.index
         lead_length = 10
         for i in range(1, lead_length + 1):
             leads[feature_name + "_lead_" + str(i)] = d[feature_name].shift(-i)
@@ -187,13 +189,16 @@ def forecast(din: pd.DataFrame, length: int, model: object, cvparam: list = None
         lead_length = leads.shape[1]
 
     imputer = SimpleImputer(strategy="median", copy=False)
+    backupimputer = SimpleImputer(fill_value = d[feature_name].median(), strategy="constant", copy=False)
     imputer.fit_transform(lags)
     imputer.fit_transform(leads)
+    backupimputer.fit_transform(lags)
+    backupimputer.fit_transform(leads)
 
     future = d[[feature_name]].join(leads)
 
     if cvparam != None:
-        model = GridSearchCV(model, *cvparam).fit(past, leads).best_estimator_
+        model = GridSearchCV(model, **cvparam).fit(lags, future).best_estimator_
 
     date = d.index.to_series().apply(lambda x: x.toordinal())
     foreDate = (np.arange(length, dtype=int) + 1) * int(date.diff().mean()) + date.iloc[-1]
@@ -202,8 +207,9 @@ def forecast(din: pd.DataFrame, length: int, model: object, cvparam: list = None
     try:
         model = RegressorChain(model)
         model.fit(lags, future)
-    except:
-        raise TypeError("Could not fit given model through an sklearn.multioutput.RegressorChain()")
+    except Exception as err:
+        print(err)
+        raise TypeError("Could not fit given model and parameters through an sklearn.multioutput.RegressorChain()")
 
     #Reserve Memory
     foreRes = pd.DataFrame(np.zeros((length, lead_length + 1)), columns=list(feature_name + "_" + str(i) for i in range(0, lead_length + 1, 1)), index=range(length))
---
+++ test_forecasthelper.py
@@ -0,0 +1,150 @@
+import pytest
+
+import forecasthelper as fh
+
+import pandas as pd
+import random as rand
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from statsmodels.tsa.deterministic import CalendarFourier, DeterministicProcess
+from sklearn.multioutput import RegressorChain
+from datetime import datetime
+from sys import maxsize
+
+def test__fixdate_basecase():
+    inputParam = pd.DataFrame({'Other': [1, 2]}, index=pd.Series([datetime(year=1970, month=1, day=1), datetime(year=1970, month=1, day=2)], name='Date'))
+    desired = 'datetime64'
+    assert fh._fixDate(inputParam).index.inferred_type == desired
+
+def test__fixdate_incolumns():
+    inputParam = pd.DataFrame({'Date': pd.Series([datetime(year=1970, month=1, day=1), datetime(year=1970, month=1, day=2)]),
+                                 'Other': [1, 2]})
+    desired = 'datetime64'
+    assert fh._fixDate(inputParam).index.inferred_type == desired
+
+def test__fixdate_integer_neat():
+    inputParam = pd.DataFrame({'Date':  pd.Series([datetime(year=1970, month=1, day=1), datetime(year=1970, month=1, day=2)]).apply(lambda x: x.value),
+                                 'Other': [1, 2]})
+    desired = 'datetime64'
+    assert fh._fixDate(inputParam).index.inferred_type == desired
+
+def test__fixdate_string_neat():
+    inputParam = pd.DataFrame({'Date':  pd.Series([datetime(year=1970, month=1, day=1), datetime(year=1970, month=1, day=2)]).apply(lambda x: x.isoformat()),
+                                 'Other': [1, 2]})
+    desired = 'datetime64'
+    assert fh._fixDate(inputParam).index.inferred_type == desired
+
+def test__fixdate_integer_random():
+    inputParam = pd.DataFrame({'Date':  [rand.randrange(maxsize), rand.randrange(maxsize)],
+                                 'Other': [1, 2]})
+    desired = 'datetime64'
+    assert fh._fixDate(inputParam).index.inferred_type == desired
+
+def test__fixdate_string():
+    inputParam = pd.DataFrame({'Date':  ["1970-01-01", "1/2/1970"],
+                                 'Other': [1, 2]})
+    desired = 'datetime64'
+    assert fh._fixDate(inputParam).index.inferred_type == desired
+
+def test__fixdate_emptyerror():
+    inputParam = pd.DataFrame()
+    with pytest.raises(ValueError):
+        fh._fixDate(inputParam)
+
+def test__fixdate_failerror():
+    inputParam = pd.DataFrame({'Date': ["hi", "bye"], 'Other': [1, 2]})
+    with pytest.raises(TypeError):
+        fh._fixDate(inputParam)
+
+def test_detrend_target():
+    inputParam = [pd.DataFrame({'Other': [1, 2]}, index=pd.Series([datetime(year=1970, month=1, day=1), datetime(year=1970, month=1, day=2)], name='Date')), "Other"]
+    dptest = DeterministicProcess(index=inputParam[0].index, constant=True, order=1, drop=True)
+    lrtest = LinearRegression(fit_intercept=False).fit(dptest.in_sample(), inputParam[0]['Other'])
+    
+    desired = [pd.DataFrame(inputParam[0]['Other'] - lrtest.predict(dptest.in_sample())), lrtest.predict(dptest.in_sample()), dptest.in_sample()]
+
+    output = fh.detrend(*inputParam)
+    assert output[0].equals(desired[0])
+    assert (output[1].predict(dptest.in_sample()) == desired[1]).all()
+    assert output[2].in_sample().equals(desired[2])
+
+def test_detrend_notarget():
+    inputParam = [pd.DataFrame({'Other': [1, 2]}, index=pd.Series([datetime(year=1970, month=1, day=1), datetime(year=1970, month=1, day=2)], name='Date')), None]
+    dptest = DeterministicProcess(index=inputParam[0].index, constant=True, order=1, drop=True)
+    lrtest = LinearRegression(fit_intercept=False).fit(dptest.in_sample(), inputParam[0]['Other'])
+    
+    desired = [pd.DataFrame(inputParam[0]['Other'] - lrtest.predict(dptest.in_sample())), lrtest.predict(dptest.in_sample()), dptest.in_sample()]
+
+    output = fh.detrend(*inputParam)
+    assert output[0].equals(desired[0])
+    assert (output[1].predict(dptest.in_sample()) == desired[1]).all()
+    assert output[2].in_sample().equals(desired[2])
+
+def test_deseason_target():
+    inputParam = [pd.DataFrame({'Other': [1, 2, 3]}, index=pd.Series([datetime(year=1970, month=1, day=1), datetime(year=1970, month=1, day=2), datetime(year=1970, month=1, day=3)], name='Date')), ('A', 2), "Other"]
+    fouriertest = CalendarFourier(freq='A', order=2)
+    dptest = DeterministicProcess(index=inputParam[0].index, constant=True, order=1, seasonal=True, additional_terms=[fouriertest], drop=True)
+    lrtest = LinearRegression(fit_intercept=False).fit(dptest.in_sample(), inputParam[0]['Other'])
+    
+    desired = [pd.DataFrame(inputParam[0]['Other'] - lrtest.predict(dptest.in_sample())), lrtest.predict(dptest.in_sample()), dptest.in_sample()]
+
+    output = fh.deseason(*inputParam)
+    assert output[0].equals(desired[0])
+    assert (output[1].predict(dptest.in_sample()) == desired[1]).all()
+    assert output[2].in_sample().equals(desired[2])
+
+def test_deseason_notarget():
+    inputParam = [pd.DataFrame({'Other': [1, 2, 3]}, index=pd.Series([datetime(year=1970, month=1, day=1), datetime(year=1970, month=1, day=2), datetime(year=1970, month=1, day=3)], name='Date')), ('A', 2), None]
+    fouriertest = CalendarFourier(freq='A', order=2)
+    dptest = DeterministicProcess(index=inputParam[0].index, constant=True, order=1, seasonal=True, additional_terms=[fouriertest], drop=True)
+    lrtest = LinearRegression(fit_intercept=False).fit(dptest.in_sample(), inputParam[0]['Other'])
+    
+    desired = [pd.DataFrame(inputParam[0]['Other'] - lrtest.predict(dptest.in_sample())), lrtest.predict(dptest.in_sample()), dptest.in_sample()]
+
+    output = fh.deseason(*inputParam)
+    assert output[0].equals(desired[0])
+    assert (output[1].predict(dptest.in_sample()) == desired[1]).all()
+    assert output[2].in_sample().equals(desired[2])
+
+def test_multi_shift_target_lagwsteps():
+    inputParam = [pd.DataFrame({'Other': [1, 2, 3]}, index=pd.Series([datetime(year=1970, month=1, day=1), datetime(year=1970, month=1, day=2), datetime(year=1970, month=1, day=3)], name='Date')), 2, [1, 2], True, "Other"]
+    desired = pd.DataFrame(index=inputParam[0].index)
+    for i in [1, 2]:
+        desired["Other_lag_" + str(i)] = inputParam[0].shift(i)
+    
+    assert fh.multi_shift(*inputParam).equals(desired)
+
+def test_multi_shift_target_lagwosteps():
+    inputParam = [pd.DataFrame({'Other': [1, 2, 3]}, index=pd.Series([datetime(year=1970, month=1, day=1), datetime(year=1970, month=1, day=2), datetime(year=1970, month=1, day=3)], name='Date')), 2, [], True, "Other"]
+    desired = pd.DataFrame(index=inputParam[0].index)
+    for i in [1, 2]:
+        desired["Other_lag_" + str(i)] = inputParam[0].shift(i)
+    
+    assert fh.multi_shift(*inputParam).equals(desired)
+
+def test_multi_shift_target_lead():
+    inputParam = [pd.DataFrame({'Other': [1, 2, 3]}, index=pd.Series([datetime(year=1970, month=1, day=1), datetime(year=1970, month=1, day=2), datetime(year=1970, month=1, day=3)], name='Date')), 2, [], False, "Other"]
+    desired = pd.DataFrame(index=inputParam[0].index)
+    for i in range(1, 3):
+        desired["Other_lead_" + str(i)] = inputParam[0].shift(-i)
+    
+    assert fh.multi_shift(*inputParam).equals(desired)
+
+def test_forecast():
+    inputParam = [pd.DataFrame({'Other': [1, 2, 3, 4, 5, 6]}, index=pd.Series([datetime(year=1970, month=1, day=1), datetime(year=1970, month=1, day=2), datetime(year=1970, month=1, day=3),
+                                                                                datetime(year=1970, month=1, day=4), datetime(year=1970, month=1, day=5), datetime(year=1970, month=1, day=6)], name='Date')),
+                    2,
+                    DecisionTreeRegressor(),
+                    {'param_grid': {'max_leaf_nodes': [2, 3]}, 'cv': 2, 'scoring': 'neg_mean_absolute_error'},
+                    'Other',
+                    pd.DataFrame(),
+                    pd.DataFrame({'Other_lead_1': [2, 3, 4, 5, 6, 4], 'Other_lead_2': [3, 4, 5, 6, 7, 5]}, index=pd.Series([datetime(year=1970, month=1, day=1), datetime(year=1970, month=1, day=2), datetime(year=1970, month=1, day=3),
+                                                                                datetime(year=1970, month=1, day=4), datetime(year=1970, month=1, day=5), datetime(year=1970, month=1, day=6)], name='Date'))]
+    
+    desired = [pd.DataFrame({'Other_0': [1, 2],'Other_1': [2, 3], 'Other_2': [3, 4]}, index=pd.Series([datetime(year=1970, month=1, day=7), datetime(year=1970, month=1, day=8)], name='Date')), RegressorChain(DecisionTreeRegressor())]
+
+    output = fh.forecast(*inputParam)
+    assert output[0].shape == desired[0].shape
+    assert (output[0].index == desired[0].index).all()
+    assert type(output[1])== type(desired[1])
+    assert output[1]._estimator_type == desired[1]._estimator_type
